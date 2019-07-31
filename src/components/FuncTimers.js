7894985e50876b9d442b1f8546f8149e2625f6ba
Sean Li <lizishuo1121@gmail.com>
2019-07-31 17:49:56 +1000

    to class component

--- src/components/FuncTimers.js
+++ src/components/FuncTimers.js
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from 'react';
 
-function FuncTimer() {
-    const [date, setDate] = useState(new Date());
+// function FuncTimer() {
+//     const [date, setDate] = useState(new Date());
 
-    useEffect(() => {
-        const timerId = setInterval(() => {
-            setDate(new Date());
-        }, 1000);
+//     useEffect(() => {
+//         const timerId = setInterval(() => {
+//             setDate(new Date());
+//         }, 1000);
             
-        return () => {
-            clearInterval(timerId);
-        };
-    }, [])
+//         return () => {
+//             clearInterval(timerId);
+//         };
+//     }, [])
 
-    return (
-        <div>{date.toLocaleTimeString()}</div>
-    );
-}
+//     return (
+//         <div>{date.toLocaleTimeString()}</div>
+//     );
+// }
 
 export default function Timers() {
     return (
